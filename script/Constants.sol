96e19b02b880dbac1ac65e69bf9ba6651f33c8d1
omurovec <owenmurovec@gmail.com>
2023-02-20 20:24:56 -0800

    feat: add constants & cleanup

---
+++ script/Constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.8.7 <0.9.0;
+
+uint256 constant L2_TX_MAX_GAS_LIMIT = 2097152;
+uint256 constant DEFAULT_L2_GAS_PRICE_PER_PUBDATA = 800;
+
+string constant ZKSOLC_BIN_REPO = "https://github.com/matter-labs/zksolc-bin";
